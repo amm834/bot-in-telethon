c4585be4619e5ce74bd2f046c268f32426bd4496
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-03-13 06:30:23 +0630

    feat: add kickout user

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def on_callback_query(event):
 async def handler(event):
     user = await event.get_user()
     if event.user_joined:
-        await event.reply(f"🐙 Welcome {user.username} to the Myanmar Cyber Youths")
+        await event.reply(f"🐙 Welcome {user.username} to the magnificent Myanmar Cyber Youths Clan")
 
 
 @client.on(events.NewMessage(pattern='/start'))
@@ -57,6 +57,24 @@ async def handle_start_command(event):
     if isinstance(chat_id, (PeerUser, PeerChat, PeerChannel)):
         await client.send_message(chat_id, 'Leaving the chat')
         await client.delete_dialog(chat_id)
+
+
+# remove use by username
+@client.on(events.NewMessage(pattern=r'/kick @(\w+)'))
+async def on_user_kick(event):
+    print(f"Received a message from {event.sender.username}: {event.message.message}")
+
+    sender = await event.get_sender()
+    chat_id = event.message.peer_id
+    permission = await client.get_permissions(chat_id, sender.id)
+    kick_username = event.pattern_match.group(1)
+
+    if not permission.is_admin:
+        await event.reply('😾 You are not allowed to use this command')
+        return
+
+    if isinstance(chat_id, (PeerUser, PeerChat, PeerChannel)):
+        await client.edit_permissions(chat_id, kick_username, view_messages=False, send_messages=False)
 
 
 try:
