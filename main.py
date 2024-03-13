d9524895728c8a43d6e3c8bdd61111f6f90e45a6
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-03-13 10:47:36 +0630

    feat: consume webhook from linear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import hashlib
 import hmac
-from typing import Union
 
 from fastapi import FastAPI, Request
 
@@ -14,22 +13,30 @@ async def read_root():
     }
 
 
-WEBHOOK_SECRET = "lin_wh_39JXATaYOqEsGJlPOyiw121RUgPsFaINmcwdGFCa8pqV"
+# WEBHOOK_SECRET = "lin_wh_39JXATaYOqEsGJlPOyiw121RUgPsFaINmcwdGFCa8pqV"
+
+WEBHOOK_SECRET = "lin_wh_g14SfvkEZOnkwQJSIE4o5SNOSK4QntPyENSg66nXjkOM"
 
 
 @app.post("/webhook")
-async def webhook_handler(event: Union[dict, list], request: Request):
-    digest = hmac.new(bytes(WEBHOOK_SECRET, 'UTF-8'),
-                      bytes(event, 'UTF-8'), hashlib.sha256)
+async def webhook_handler(request: Request):
+    rawBody = await request.body()
+    rawBody = rawBody.decode('utf-8')
+    body = await request.json()
+
+    digest = hmac.new(
+        bytes(WEBHOOK_SECRET, 'UTF-8'),
+        msg=bytes(rawBody, 'UTF-8'),
+        digestmod=hashlib.sha256)
+
     signature = digest.hexdigest()
 
     if signature != request.headers.get('linear-signature'):
         return {
-            "message": "Invalid signature",
-            "event": event
+            "message": "Invalid signature"
         }
 
     return {
         "message": "Webhook received",
-        "event": event
+        "data": body,
     }
