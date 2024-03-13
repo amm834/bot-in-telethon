a72b9194b56c52b5bf5ae70466c37846b325ab1e
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-03-13 09:48:32 +0630

    feat: listen linear webhook

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import hashlib
+import hmac
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -12,8 +14,21 @@ async def read_root():
     }
 
 
+WEBHOOK_SECRET = "lin_wh_39JXATaYOqEsGJlPOyiw121RUgPsFaINmcwdGFCa8pqV"
+
+
 @app.post("/webhook")
-async def webhook_handler(event: Union[dict, list]):
+async def webhook_handler(event: Union[dict, list], request: Request):
+    digest = hmac.new(bytes(WEBHOOK_SECRET, 'UTF-8'),
+                      bytes(event, 'UTF-8'), hashlib.sha256)
+    signature = digest.hexdigest()
+
+    if signature != request.headers.get('linear-signature'):
+        return {
+            "message": "Invalid signature",
+            "event": event
+        }
+
     return {
         "message": "Webhook received",
         "event": event
